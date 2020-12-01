253c566697e33835a10b73b24b9a755bfc1d5390
Amit Thapliyal <sharma.varun675@gmail.com>
2020-12-01 23:15:30 +0530

    Deploy on heroku

--- spacex-launch/server.js
+++ spacex-launch/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/spacex-launch'));
+ngApp.use(express.static('./dist'));
 ngApp.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, '/dist/index.html'));
 });
