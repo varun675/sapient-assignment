4a9904dbd6be42d69932e2856cab21dcc75c08c7
Amit Thapliyal <sharma.varun675@gmail.com>
2020-12-01 22:57:37 +0530

    Deploy on heroku

--- spacex-launch/README.md
+++ spacex-launch/README.md
@@ -1,6 +1,6 @@
 # SpacexLaunch
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.2.2.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli).
 
 # App Overview
 
--- spacex-launch/server.js
+++ spacex-launch/server.js
@@ -3,6 +3,6 @@ const path = require('path');
 const ngApp = express();
 ngApp.use(express.static('./dist/spacex-launch'));
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/spacex-launch/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
